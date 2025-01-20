5227229de8465227134a1b2596c666c41576e218
猫猫鹰 <w84627302@gmail.com>
2025-01-21 01:41:51 +0800

    [web]重新设置layout

--- web/image_id_finder/src/layout/AppLayout.vue
+++ web/image_id_finder/src/layout/AppLayout.vue
@@ -1,34 +1,19 @@
+<script setup lang="ts">
+import AppMenu from '@/components/AppMenu.vue';
+import TopBar from "@/components/TopBar.vue";
+</script>
+
 <template>
-  <div class="container-main w-full">
-    <div class="header-line flex justify-center items-center h-10 bg-blue-400 fixed top-0 left-0 w-full z-10">
-      <span class="text-white">资源ID寻找器</span>
-      <el-menu
-        background-color="rgb(96,165,250)"
-        text-color="#fff"
-        active-text-color="#fff"
-        router
-        class="el-menu-demo ml-auto h-10 pr-4 sm:hidden block"
-        mode="horizontal"
-        :ellipsis="false"
-      >
-        <el-menu-item index="maimai-icon">头像</el-menu-item>
-        <el-menu-item index="maimai-plate">姓名框</el-menu-item>
-      </el-menu>
-
-
-          </div>
-      <AppMenu/>
-    <div class="image-container sm:ml-48 sm:p-4 pt-10 mt-10">
-      <router-view/>
-      </div>
-
+  <div class="w-100vw h-100vh fixed">
+    <TopBar class="fixed w-full h-16 z-10 border-solid b-0 b-b-1 border-gray-600" />
+    <AppMenu class="fixed w-48 h-full mt-16 hidden md:block" />
+    <el-scrollbar
+      class="mt-16 md:ml-48 h-full pb-16"
+    >
+      <router-view />
+    </el-scrollbar>
   </div>
 </template>
 
-<script setup lang="ts">
-import AppMenu from '@/components/AppMenu.vue';
-</script>
-
-
-
-
+<style scoped>
+</style>
